e5e4385117184d4e90944c0ea494a98314edb1a7
Rodrigo Machado <rml22388@gmail.com>
2025-04-28 23:01:40 -0400

    code linting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,11 +1,15 @@
+"""Provides methods to detect emotion from text."""
 import requests
 
 def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    """Detects emotion from a given text."""
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1' + \
+        '/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyze } }
 
-    r = requests.post(url, json=data, headers=headers)
+    r = requests.post(url, json=data, headers=headers, timeout=10)
 
     if r.status_code == 400:
         return {
@@ -20,4 +24,3 @@ def emotion_detector(text_to_analyze):
     emotions = r.json()['emotionPredictions'][0]['emotion']
     emotions['dominant_emotion'] = sorted(emotions, key=emotions.get, reverse=True)[0]
     return emotions
-
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Emotion detector web server."""
 from flask import Flask, request, render_template
 from emotion_detection import emotion_detector
 
@@ -5,10 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    """Renders the home page."""
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def detect_emotion():
+    """Analyzes the provided text to identify emotions."""
     text_to_analyze = request.args.get('textToAnalyze', '')
     result = emotion_detector(text_to_analyze)
     dominant_emotion = result['dominant_emotion']

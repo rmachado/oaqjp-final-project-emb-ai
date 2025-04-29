6a293d16e22d530bccf4c4b4acce88f59cec62df
Rodrigo Machado <rml22388@gmail.com>
2025-04-28 22:42:07 -0400

    error handling

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,6 +6,17 @@ def emotion_detector(text_to_analyze):
     data = { "raw_document": { "text": text_to_analyze } }
 
     r = requests.post(url, json=data, headers=headers)
+
+    if r.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     emotions = r.json()['emotionPredictions'][0]['emotion']
     emotions['dominant_emotion'] = sorted(emotions, key=emotions.get, reverse=True)[0]
     return emotions
--- server.py
+++ server.py
@@ -12,6 +12,10 @@ def detect_emotion():
     text_to_analyze = request.args.get('textToAnalyze', '')
     result = emotion_detector(text_to_analyze)
     dominant_emotion = result['dominant_emotion']
+
+    if dominant_emotion is None:
+        return 'Invalid text! Please try again!'
+
     emotions = [f"'{k}': {v}"  for k,v in result.items() if k != 'dominant_emotion']
     return f'For the given statement, the system response is {", ".join(emotions)}. ' + \
         f'The dominant emotion is {dominant_emotion}'
